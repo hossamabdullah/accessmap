a9374867466570d6f218484eba3f7d8c4a24843a
Hossameldeen <hossamabdalh@gmail.com>
2018-12-10 14:37:39 +0200

    more intialization and cleaning for the code structure to our format

--- app/views.py
+++ app/views.py
@@ -26,7 +26,7 @@ class UserService(Resource):
 @api.route('/place', endPoint='PlaceApi')
 class PlaceService(Resource):
 
-    @api.doc(params={'id':'id of the user to search on'})
+    @api.doc(params={'id':'id of the place to search on'})
     def get(self):
         self.parser = reqparse.RequestParser()
         self.parser.add_argument('id', required=True, help="keyword is required")
@@ -34,4 +34,14 @@ class PlaceService(Resource):
         id = args["keyword"]
         return ("user id = "+id), 200
 
-    
+
+@api.route('/review', endPoint='ReviewApi')
+class ReviewService(Resource):
+
+    @api.doc(params={'id':'id of the review to search on'})
+    def get(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('id', required=True, help="keyword is required")
+        args = self.parser.parse_args()
+        id = args["keyword"]
+        return ("user id = "+id), 200
